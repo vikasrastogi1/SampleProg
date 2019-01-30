c798d8c62604db16f0cbc338795a09de7322ebfe
vikasrastogi1 <vrastogi1@gmail.com>
2019-01-30 22:42:18 +0530

    Update Solution.java

--- Solution.java
+++ Solution.java
@@ -1,6 +1,6 @@
 class Solution {
   public static void main(String args[]) {
-    System1.out.println("Hello World!");
+    System.out.println("Hello World!");
      System.out.println("Hello Poll SCM!");
   }
 }
